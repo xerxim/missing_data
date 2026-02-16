91d8183b86568a7109777dda4a7ecc987702c5ae
Simon Fehrenbach <simon.fehrenbach@fehrix.de>
2026-02-16 15:03:12 +0100

    Added main analysis file.

---
+++ project/main.R
@@ -0,0 +1,12 @@
+# Place for main analysis code.
+# Imports:
+## Libraries.
+library(ggplot2)
+library(mice)
+## Self written code.
+
+# Working constants:
+plot_path <- "project/plots/" # Use file.path(plot_path, "plotname") to safe.
+
+# Analysis:
+
