7cffff3e656ce1238c97c8f3085ac2be06b3c738
Alice <alice.kar.0912@gmail.com>
2026-02-17 23:31:45 +0100

    in graph_functions liegt jetzt auch eine coverage_graoh function, die funktioniert bisher aber nur für den linearen generationsprozess

--- project/dev/Alice/graph_functions.R
+++ project/dev/Alice/graph_functions.R
@@ -1,3 +1,7 @@
+
+
+library("patchwork")
+
 ### Bias Boxplot Funktion
 # Nimmt einen dataframe entgegen, wie er von mc_study() produziert wird
 bias_boxplot <- function(df, xticks = c("β0", "β1", "β2", "μ(X3)"), title = ""){
@@ -21,11 +25,127 @@ bias_boxplot <- function(df, xticks = c("β0", "β1", "β2", "μ(X3)"), title = 
 
 
 ### coverage plot function
-coverage_plot <- function(dfs, miss_perc, plot_names,
+# Funktioniert noch nicht für die Interaktioneffekte
+coverage_plot <- function(dfs, miss_perc, plot_names = c("β0", "β1", "β2", "μ(X3)"),
                           xlab, ylab) {
  
+  dfs_raw <- lapply(seq_along(dfs), function(k) {
+    dfs[[k]] %>%
+      dplyr::group_by(method, term) %>%
+      dplyr::summarise(coverage = mean(as.numeric(cover), na.rm = TRUE),
+                      .groups = "drop") %>%
+      dplyr::mutate(missperc = miss_perc[k])
+  })
 
-   
+
+  dfs_combined <- bind_rows(dfs_raw, .id = "source_id")
+
+  #List with all data split by Parameter
+  dfs_parameter_info <- dfs_combined %>% group_split(term, .keep = TRUE)
+
+  plot_names <- plot_names
+
+  #List with coverage plots for each parameter
+  plots_dfs <- vector("list", length(dfs_parameter_info))
+
+  for (i in seq_along(dfs_parameter_info)) {
+    plots_dfs[[i]] <- ggplot(dfs_parameter_info[[i]]) +
+      geom_line(aes(x = missperc, y = coverage, color = method), alpha = 0.4) +
+      geom_point(aes(x = missperc, y = coverage, color = method)) +
+      geom_hline(yintercept = 0.9, linetype = 2) +
+      coord_cartesian(ylim = c(0, 1)) +
+      labs(title = plot_names[i], x = xlab, y = NULL) +
+      theme_classic()
+  }
+
+
+  # 1) Add y-axis title only to the leftmost plot
+  plots_dfs[[1]] <- plots_dfs[[1]] + labs(y = "Coverage")
+
+  # 2) Add custom info to the rightmost graph (top-right corner)
+  plots_dfs[[4]] <- plots_dfs[[4]] +
+    scale_y_continuous(
+      name = NULL,
+      # Right side: duplicate scale but only use it for the title
+      sec.axis = dup_axis(name = ylab)
+   ) + theme(
+      axis.text.y.right  = element_blank(),
+      axis.ticks.y.right = element_blank(),
+      axis.line.y.right = element_blank(),
+      axis.title.y.right = element_text(margin = margin(l = 8)))
+
+  plots_finished <- plots_mcar_x3_missing[[1]] +  plots_mcar_x3_missing[[2]] +
+    plots_mcar_x3_missing[[3]] + plots_mcar_x3_missing[[4]] +
+    plot_layout(ncol = 4,
+                guides = "collect",
+                axis_titles = "collect") &
+    theme(legend.position = "bottom")
+
+  plots_finished 
   
+}
+
+
+
+
+
+### coverage plot function
+##der will den 5. plot noch nicht....
+coverage_plot_int <- function(dfs, miss_perc, plot_names = c("β0", "β1", "β2", "β1*β2", "μ(X3)"),
+                              xlab, ylab) {
+  
+  dfs_raw <- lapply(seq_along(dfs), function(k) {
+    dfs[[k]] %>%
+      dplyr::group_by(method, term) %>%
+      dplyr::summarise(coverage = mean(as.numeric(cover), na.rm = TRUE),
+                       .groups = "drop") %>%
+      dplyr::mutate(missperc = miss_perc[k])
+  })
+  
+  
+  dfs_combined <- bind_rows(dfs_raw, .id = "source_id")
+  
+  #List with all data split by Parameter
+  dfs_parameter_info <- dfs_combined %>% group_split(term, .keep = TRUE)
+  
+  plot_names <- plot_names
+  
+  #List with coverage plots for each parameter
+  plots_dfs <- vector("list", length(dfs_parameter_info))
+  
+  for (i in seq_along(dfs_parameter_info)) {
+    plots_dfs[[i]] <- ggplot(dfs_parameter_info[[i]]) +
+      geom_line(aes(x = missperc, y = coverage, color = method), alpha = 0.4) +
+      geom_point(aes(x = missperc, y = coverage, color = method)) +
+      geom_hline(yintercept = 0.9, linetype = 2) +
+      coord_cartesian(ylim = c(0, 1)) +
+      labs(title = plot_names[i], x = xlab, y = NULL) +
+      theme_classic()
+  }
+  
+  
+  # 1) Add y-axis title only to the leftmost plot
+  plots_dfs[[1]] <- plots_dfs[[1]] + labs(y = "Coverage")
+  
+  # 2) Add custom info to the rightmost graph (top-right corner)
+  plots_dfs[[5]] <- plots_dfs[[5]] +
+    scale_y_continuous(
+      name = NULL,
+      # Right side: duplicate scale but only use it for the title
+      sec.axis = dup_axis(name = ylab)
+    ) + theme(
+      axis.text.y.right  = element_blank(),
+      axis.ticks.y.right = element_blank(),
+      axis.line.y.right = element_blank(),
+      axis.title.y.right = element_text(margin = margin(l = 8)))
+  
+  plots_finished <- plots_mcar_x3_missing[[1]] +  plots_mcar_x3_missing[[2]] +
+    plots_mcar_x3_missing[[3]] + plots_mcar_x3_missing[[4]] + plots_mcar_x3_missing[[5]] +
+    plot_layout(ncol = 5,
+                guides = "collect",
+                axis_titles = "collect") &
+    theme(legend.position = "bottom")
+  
+  plots_finished 
   
 }
--- project/dev/Alice/nonlinear_data.R
+++ project/dev/Alice/nonlinear_data.R
@@ -49,6 +49,29 @@ generate_data_nonlinear_strong <- function(n, seed = NA) {
 
 ### Anwendung
 
+dfs_mcar_X3_miss_nonlin <- list()
+i <- 1
+
+for (mis_rate in c(0.1, 0.2, 0.3, 0.4, 0.5)) {
+  mc_cycles <- 5
+  
+  dfs_mcar_X3_miss_nonlin[[i]] <- mc$mc_study(
+    methods = c("cart", "cart_boot", "pmm"), m = 30, formula = "X3 ~ X1*X2",
+    true_vals = c("(Intercept)"=5, "X1"=0.6, "X2"=0.5, "X1:X2"=0.1), 
+    data_generator = generate_data_nonlinear_weak,
+    n = 500, cycles = mc_cycles, miss_vars = "X3", true_means = c("X3" = 4.55),
+    miss = "MCAR", miss_rates =  0.3, miss_aux = NULL, seed = NA
+  )
+  
+  i <- i+1
+  
+}
+
+#coverage_plot_int(dfs_mcar_X3_miss_nonlin, miss_perc = c(10, 20, 30, 40, 50), 
+              #plot_names = c("β0", "β1", "β2", "β1*β2", "μ(X3)"), xlab = "Missing in X3 (%)", 
+              #ylab = "MCAR,\n only missings in X3")
+
+
 nonlinear_weak_interaction <- mc$mc_study(
   methods = c("cart", "cart_boot", "pmm"), m = 30, formula = "X3 ~ X1*X2",
   true_vals = c("(Intercept)"=5, "X1"=0.6, "X2"=0.5, "X1:X2"=0.1), 
